1ebf45f001875d78597850f3b9fe34776bac1443
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-10-09 20:01:52 +0300

    Add main dictionary getter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-export const example = (): string => 'test string';
+import path from 'path';
+import { DictionaryEntry } from './models';
+import { read } from './reader';
 
-export default {
-  example,
-};
+const DICTIONARY_PATH = path.join(`${__dirname}/../resources/old-swedish-dictionary.json`);
+
+export function getDictionary() : DictionaryEntry[] {
+  const words = read(DICTIONARY_PATH);
+
+  return words;
+}
+
+export { DictionaryEntry } from './models';
