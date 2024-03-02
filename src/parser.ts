0e00a9018bdc52e39e18dcb723be332cc5a6d177
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-03-02 19:13:00 +0200

    Dictionary source: use gzipped variant
    Replace crudely minified JSON source with gzipped JSON source.
    
    Is much smaller in size and removes the need to use "simplified" keys, that would in turn need to be parsed back.
    
    While unzipping the gzipped content back to json introduces some overhead, the overall performance is speed wise more or less identical. However, as the asset being processed is notably smaller in size, it should have in overall very positive performance effect in memory footprint & transfer time over wire.

--- src/parser.ts
+++ src/parser.ts
@@ -1,31 +1,12 @@
+import zlib from 'zlib';
 import { DictionaryEntry } from './models';
 
-const KEYS_TO_ENTRY_KEYS = new Map();
-KEYS_TO_ENTRY_KEYS.set('a', 'headword');
-KEYS_TO_ENTRY_KEYS.set('b', 'partOfSpeech');
-KEYS_TO_ENTRY_KEYS.set('c', 'grammaticalAspect');
-KEYS_TO_ENTRY_KEYS.set('d', 'information');
-KEYS_TO_ENTRY_KEYS.set('e', 'definitions');
-KEYS_TO_ENTRY_KEYS.set('f', 'alternativeForms');
+export const parse = (content: Buffer): DictionaryEntry[] => {
+  // Decompress the gzipped data.
+  const decompressedData = zlib.gunzipSync(content);
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function parseEntry(this: any, key: string, value: unknown): unknown {
-  const parsedKey = KEYS_TO_ENTRY_KEYS.get(key);
-  if (parsedKey) {
-    this[parsedKey] = value;
-    return;
-  }
-  // eslint-disable-next-line consistent-return
-  return value;
-}
-
-export const parse = (content: Buffer) : DictionaryEntry[] => {
-  const words = JSON.parse(
-    content.toString(),
-    parseEntry,
-  );
-
-  return words;
+  // Parse the JSON content.
+  return JSON.parse(decompressedData.toString());
 };
 
 export default {
